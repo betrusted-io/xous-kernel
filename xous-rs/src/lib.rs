6fc905467806d8bab3b36fea35d04b8e7e73ca89
Sean Cross <sean@xobs.io>
2020-02-20 16:35:20 +0800

    syscall: return a Result<> from syscalls
    Signed-off-by: Sean Cross <sean@xobs.io>

--- xous-rs/src/lib.rs
+++ xous-rs/src/lib.rs
@@ -42,7 +42,7 @@ pub enum XousResult {
 
 pub type XousError = u32;
 
-pub type SyscallResult = XousResult;
+pub type SyscallResult = Result<XousResult, XousError>;
 // type SyscallResult = [u32; 8];
 
 extern "Rust" {
@@ -51,17 +51,23 @@ extern "Rust" {
     // fn _xous_syscall(nr: u32, a1: u32, a2: u32, a3: u32, a4: u32, a5: u32, a6: u32, a7: u32) -> CallArgs;
     // fn _xous_syscall(nr: u32, a1: u32, a2: u32, a3: u32, a4: u32, a5: u32, a6: u32, a7: u32) -> Result<XousResult, XousError>;
     // fn _xous_syscall(args: [u32; 8]) -> [u32; 8];
-    fn _xous_syscall(nr: u32, a1: u32, a2: u32, a3: u32, a4: u32, a5: u32, a6: u32, a7: u32, ret: &mut SyscallResult);
+    fn _xous_syscall(nr: u32, a1: u32, a2: u32, a3: u32, a4: u32, a5: u32, a6: u32, a7: u32, ret: &mut XousResult);
 }
+
+use core::mem::MaybeUninit;
 
 pub fn syscall(args: SyscallArguments) -> SyscallResult {
     // let a = CallArgs::Args(args.nr, args.a1, args.a2, args.a3, args.a4, args.a5, args.a6, args.a7);
     // unsafe { _xous_syscall(args) }
     // let mut ret = [0; 8];
-    let mut ret = XousResult::XousError(42);
+    let mut ret = unsafe { MaybeUninit::uninit().assume_init() };
 
     unsafe { _xous_syscall(args.nr, args.a1, args.a2, args.a3, args.a4, args.a5, args.a6, args.a7, &mut ret) };
-    ret
+    match ret {
+        // XousResult::XousError(e) => Err(e),
+        other => Ok(other)
+    }
+    // Ok(ret)
     // unsafe { _xous_syscall(args.nr, args.a1, args.a2, args.a3, args.a4, args.a5, args.a6, args.a7) }
     // unsafe { _xous_syscall([args.nr, args.a1, args.a2, args.a3, args.a4, args.a5, args.a6, args.a7]) }
 }
