810e7869d56168d9dde601d340b3d63c953368be
Sean Cross <sean@xobs.io>
2020-02-25 09:22:11 +0800

    asm: move to src/ directory
    Signed-off-by: Sean Cross <sean@xobs.io>

--- assemble.sh
+++ assemble.sh
@@ -8,14 +8,14 @@ crate=xous-kernel
 mkdir -p bin
 rm -f bin/*.a
 
-riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac asm.S -o bin/$crate.o
+riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32imac src/asm.S -o bin/$crate.o
 ar crs bin/riscv32imac-unknown-none-elf.a bin/$crate.o
 ar crs bin/riscv32imc-unknown-none-elf.a bin/$crate.o
 
-riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i asm.S -DSKIP_MULTICORE -o bin/$crate.o
+riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32i src/asm.S -DSKIP_MULTICORE -o bin/$crate.o
 ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
 
-riscv64-unknown-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac asm.S -o bin/$crate.o
+riscv64-unknown-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64imac src/asm.S -o bin/$crate.o
 ar crs bin/riscv64imac-unknown-none-elf.a bin/$crate.o
 ar crs bin/riscv64gc-unknown-none-elf.a bin/$crate.o
 
